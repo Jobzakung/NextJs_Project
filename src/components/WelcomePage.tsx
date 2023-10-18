13381be557d8b9089c87855398d8e0a024d0ab20
jobzakung <jobzakung@naver.com>
2023-10-19 01:53:30 +0700

    fix issue

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -9,13 +9,13 @@ const WelcomePage = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-center mt-[150px] mb-[50px] 2xl:mt-[150px] lg:mt-[25px] lg:mb-[20px]">
+      <div className="flex justify-center mt-[150px] mb-[50px] 2xl:mt-[40px] lg:mt-[25px] md:mt-[10px] sm:mt-[10px] 2xl:mb-[40px] lg:mb-[30px] md:mb-[10px] sm:mb-[10px]">
         <p className="text-blue-700 font-kanit text-4xl font-semibold">
           แบบทดสอบความรู้
         </p>
       </div>
       <div className="flex justify-center items-center m-auto">
-        <video width="1280" height="720" controls poster="/PosterVideo.png">
+        <video className="w-[1280px]" controls poster="/PosterVideo.png">
           <source
             src="/video/Vi1.mp4"
             type="video/mp4"
